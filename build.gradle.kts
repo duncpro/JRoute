120d64967b81a8b1d65c93a75e9213d636758d41
Duncan Proctor <dbp19a@my.fsu.edu>
2021-09-23 09:21:31 -0400

    publish to codeartifact
    Took 50 seconds

--- build.gradle.kts
+++ build.gradle.kts
@@ -45,4 +45,13 @@ publishing {
             from(components["java"])
         }
     }
+    repositories {
+        maven {
+            url = uri("https://duncpro-personal-618824625980.d.codeartifact.us-east-1.amazonaws.com/maven/duncpro-personal/")
+            credentials {
+                username = "aws"
+                password = System.getenv("CODEARTIFACT_AUTH_TOKEN")
+            }
+        }
+    }
 }
