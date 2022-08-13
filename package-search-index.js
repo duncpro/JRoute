f2b719e524013ab7eeeb1876c96a0b42f3ca4bbd
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2022-08-13 23:27:47 +0000

    Deploy duncpro/JRoute to github.com/duncpro/JRoute.git:gh-pages

--- package-search-index.js
+++ package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.duncpro.jroute"},{"l":"com.duncpro.jroute.route"},{"l":"com.duncpro.jroute.router"},{"l":"com.duncpro.jroute.util"}]
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.duncpro.jroute"},{"l":"com.duncpro.jroute.rest"},{"l":"com.duncpro.jroute.route"},{"l":"com.duncpro.jroute.router"},{"l":"com.duncpro.jroute.util"}]
